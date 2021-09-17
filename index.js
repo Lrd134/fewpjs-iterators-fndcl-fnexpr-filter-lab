98ea1f0a74ecd4f12d2ab90cdf2920bfb18b185b
Lrd134 <larryc3200@gmail.com>
2021-09-17 15:24:52 -0400

    commit tree b2c3ef451f4ff0a7045f98369f6c491e49a99cfe

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (drivers, string) => {
+  return drivers.filter(name =>{
+    return name.toUpperCase() === string.toUpperCase();
+  });
+}
+
+const fuzzyMatch = (drivers, string) => {
+  return drivers.filter(name => {
+    return name.slice(0, string.length).toUpperCase() === string.toUpperCase();
+  });
+}
+
+const matchName = (drivers, string) => {
+  return drivers.filter(obj => {
+    return obj.name === string;
+  });
+}
